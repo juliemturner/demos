7c8ee3227d7ed3898be2dbe95294f904ddec1692
juliemturner <jturner@bluemetal.com>
2016-10-19 10:52:26 -0400

    SEF Demo Additions

---
+++ ClientSideDev/BasicAngular/basicangular.js
@@ -0,0 +1,49 @@
+"use strict";
+var BA = window.BA || {};
+BA.currentSite = window.location.protocol + "//" + window.location.host + _spPageContextInfo.webServerRelativeUrl;
+
+var myModule = angular.module('myApp', []);
+
+myModule.factory("myAppService", function ($http) {
+    var getData = function(list){
+        return $http({
+            method: 'GET',
+			headers: {
+				'Content-Type': 'application/json;odata=verbose',
+				'accept': 'application/json;odata=verbose'
+			},
+            url: BA.currentSite + "/_api/web/lists/getbytitle('" + list + "')/items?$top=5000&$select=ID,BusinessUnit,Category,Status,DueDate,AssignedTo/Title&$expand=AssignedTo/Title"
+        });
+    };
+
+    return {
+        getData: getData
+    };
+    
+});
+
+myModule.controller('myAppCtrl', ['$scope','$timeout','myAppService', 
+function myAppCtrl($scope, $timeout, myAppService) {
+    var REQUESTS_LIST = "IT Requests";
+    $scope.requests = [];
+    
+    myAppService.getData(REQUESTS_LIST).then(function (response) {
+        var data = response.data.d.results;
+        if(data.length > 0){
+            for (var i = 0; i < data.length; i++) {
+                var item = {};
+                item.ID = data[i].ID;
+                item.BusinessUnit = data[i].BusinessUnit;
+                item.Category = data[i].Category;
+                item.Status = data[i].Status;
+                if(data[i].DueDate != undefined)
+                    item.DueDate = new Date(data[i].DueDate);
+                if(data[i].AssignedTo != undefined)
+                    item.Assigned = data[i].AssignedTo.Title.split(" ")[0];
+                //Build an array
+                $scope.requests.push(item);
+            }
+            $timeout(function() {}, 0);
+        }
+    });
+}]);
